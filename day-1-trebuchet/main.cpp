67eb6222a2e72dc721faa26289a8273ce9cf4544
Kiet Pham <kpham@axon.com>
2024-12-31 15:29:05 +0700

    small refactor to support two parts

--- day-1-trebuchet/main.cpp
+++ day-1-trebuchet/main.cpp
@@ -39,7 +39,32 @@ int try_convert_to_number(const string& str, unsigned int pos, unsigned int& num
     return -1;
 }
 
-tuple<unsigned int, unsigned int> get_first_last_numbers_from_string(const string& str) {
+tuple<unsigned int, unsigned int> part_one_get_pair_of_first_last_numbers(const string& str) {
+    unsigned int first_digit, last_digit;
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        const auto c = *it;
+        if (('0' <= c) && (c <= '9')) {
+            // Fast conversion using ASCII. I hate using library just for this :-)
+            first_digit = c - 48;
+            break;
+        }
+    }
+
+    // Yes, DO THIS TO REVERSE ITERATING. DO NOT use begin(), end() because it shall
+    // miss the first element!!!. Refer to https://cplusplus.com/reference/string/string/end/
+    // and https://cplusplus.com/reference/string/string/begin/ to know why
+    for (auto it = str.rbegin(); it != str.rend(); ++it) {
+        const auto c = *it;
+        if (('0' <= c) && (c <= '9')) {
+            // Fast conversion using ASCII. I hate using library just for this :-)
+            last_digit = c - 48;
+            break;
+        }
+    }
+    return {first_digit, last_digit};
+}
+
+tuple<unsigned int, unsigned int> part_two_get_pair_of_first_last_numbers(const string& str) {
     unsigned int first_digit, last_digit;
     for (auto it = str.begin(); it != str.end(); ++it) {
         const auto c = *it;
@@ -80,15 +105,16 @@ int main(int argc, char* argv[]) {
     }
     ifstream input_file(argv[1]);
     string str;
-    unsigned long int result = 0;
+    unsigned long int part_one_result = 0;
+    unsigned long int part_two_result = 0;
     while (getline(input_file, str)) {
-        const auto pair_of_number = get_first_last_numbers_from_string(str);
-        auto first_digit = get<0>(pair_of_number);
-        auto second_digit = get<1>(pair_of_number);
-        auto number = get<0>(pair_of_number) * 10 + get<1>(pair_of_number);
-        result += number;
+        const auto part_one_pair_of_numbers = part_one_get_pair_of_first_last_numbers(str);
+        const auto part_two_pair_of_numbers = part_two_get_pair_of_first_last_numbers(str);
+        part_one_result += get<0>(part_one_pair_of_numbers) * 10 + get<1>(part_one_pair_of_numbers);
+        part_two_result += get<0>(part_two_pair_of_numbers) * 10 + get<1>(part_two_pair_of_numbers);
     }
-    printf("Result is %lu\n", result);
+    printf("Part 1 result is %lu\n", part_one_result);
+    printf("Part 2 result is %lu\n", part_two_result);
     input_file.close();
     return 0;
 }
